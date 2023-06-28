0ed9769ab3c01c942db5c7400caa03139f2b5d0b
yunyeonjeong <duswjddl0817@naver.com>
2023-06-28 16:00:29 +0900

    230628 1600 수정

--- about.js
+++ about.js
@@ -1,5 +1,38 @@
+//Loading
+const loader = document.querySelector(".loader");
+const html = document.querySelector("html");
+
+html.style.overflow = "hidden"; //로딩 중 스크롤 방지
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    //  <-* 로딩속도를 구현하기 위한 코드로 실제 적용시 제거
+
+    loader.style.opacity = "0";
+    html.style.overflow = "auto"; //스크롤 방지 해제
+
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 400);
+  }, 2000); // <-* 로딩속도 구현
+});
+
+/*const loader = $(".loader");
+const html = $("html");
+
+html.css({ overflow: "hidden" }); //로딩 중 스크롤 방지
+
+$(window).on("load", () => {
+  setTimeout(() => {
+    //  <-* 로딩속도 구현
+
+    loader.fadeOut(300);
+    html.css({ overflow: "auto" }); //스크롤 방지 해제
+  }, 2000); //  <-* 로딩속도 구현
+});*/
+
 // 하단 소개
-(function sBox() {
+/*(function sBox() {
   let text = document.querySelector(".text");
   let tBox1 = document.querySelector(".t-box1");
   let tBox2 = document.querySelector(".t-box2");
@@ -41,4 +74,4 @@
       ImgBox.style.transform = "translateY(-150px)";
     }
   });
-})();
+})();*/
