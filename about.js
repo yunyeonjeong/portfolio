26b1c4827e5eef8ec2d1608ea038ac13733d3d4f
yunyeonjeong <duswjddl0817@naver.com>
2023-06-29 23:02:02 +0900

    230629 2302 수정

--- about.js
+++ about.js
@@ -1,40 +1,28 @@
 //Loading
-const loader = document.querySelector('.loader');
-const html = document.querySelector('html');
+const loader = document.querySelector(".loader");
+const html = document.querySelector("html");
 
-html.style.overflow = 'hidden';
+html.style.overflow = "hidden";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   setTimeout(() => {
-    loader.style.opacity = '0';
-    html.style.overflow = 'auto';
+    loader.style.opacity = "0";
+    html.style.overflow = "auto";
 
     setTimeout(() => {
-      loader.style.display = 'none';
+      loader.style.display = "none";
     }, 400);
   }, 2000);
 });
 
 // 하단 소개
-$(window).on('scroll', () => {
-  if ('.textBox'.offsetTop > '#t-box1'.offsetTop) {
-    $('#t-box1').css({ opacity: 1 });
-  } else {
-    $('#t-box1').css({ opacity: 0 });
-  }
-});
 
-/*$(window).on('scroll', () => {
+$(window).on("scroll", () => {
   let scrollHt = $(document).scrollTop();
 
   if (scrollY > 200) {
-    $('#t-box1').css({ opacity: 1 });
+    $("#t-box1").css({ opacity: 1 });
   } else {
-    $('#t-box1').css({ opacity: 0 });
+    $("#t-box1").css({ opacity: 0 });
   }
-  if (scrollY > 200) {
-    $('#t-box2').css({ opacity: 1 });
-  } else {
-    $('#t-box2').css({ opacity: 0 });
-  }
-});*/
+});
