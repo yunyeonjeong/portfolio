fc03046a360fb372f30868ab8815129a4a89e3df
yunyeonjeong <duswjddl0817@naver.com>
2023-07-30 23:17:19 +0900

    230730 2317 수정

--- kctv/index.js
+++ kctv/index.js
@@ -1,11 +1,22 @@
 // 메인 자동 슬라이더 및 버튼 슬라이더
-const mySwiper = new Swiper('.mySwiper', {
+const mySwiper = new Swiper(".mySwiper", {
   autoplay: {
     delay: 3000,
     centeredSlides: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+});
+
+// 자주하는 질문 상세 슬라이드
+const lis = document.querySelectorAll(".question-box > li");
+lis.forEach(function (a, idx) {
+  $(a).on("click", () => {
+    $(a).find(".question-wrapper").slideToggle();
+    $(a).siblings().find(".question-wrapper").slideUp();
+    $(a).toggleClass("rotate");
+    $(a).siblings().removeClass("rotate");
+  });
 });
