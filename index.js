4dd688896d5cd849f39555c0267973d8b2b7319b
yunyeonjeong <duswjddl0817@naver.com>
2023-07-31 17:56:25 +0900

    230731 1756 수정

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-//arrow
-
-/*$(document).ready(function () {
-  Moving();
-});
-function Moving() {
-  $('.box5').animate({ marginBottom: '100px' }, 300, '', function () {
-    $(this).animate({ margin: '0px' }, 300, '', function () {
-      Moving();
-    });
-  });
-}*/
-
 //메인 시간
-const date = document.querySelector(".date span");
-const hours = document.querySelector(".hours span");
-const minutes = document.querySelector(".minutes span");
-const seconds = document.querySelector(".seconds span");
+const date = document.querySelector('.date span');
+const hours = document.querySelector('.hours span');
+const minutes = document.querySelector('.minutes span');
+const seconds = document.querySelector('.seconds span');
 const now = new Date();
 
-// 재귀함수 방법
 function updateTime() {
   const now = new Date();
-  date.textContent = String(now.getDate()).padStart(2, "0");
-  hours.textContent = String(now.getHours()).padStart(2, "0");
-  minutes.textContent = String(now.getMinutes()).padStart(2, "0");
-  seconds.textContent = String(now.getSeconds()).padStart(2, "0");
+  date.textContent = String(now.getDate()).padStart(2, '0');
+  hours.textContent = String(now.getHours()).padStart(2, '0');
+  minutes.textContent = String(now.getMinutes()).padStart(2, '0');
+  seconds.textContent = String(now.getSeconds()).padStart(2, '0');
 
-  setTimeout(updateTime, 1000); // setInterval()을 이용하는 것보다 메모리 측면에서 효율적
+  setTimeout(updateTime, 1000);
 }
 updateTime();
+
+//font 위로 올라 오는 값
+AOS.init();
--- kctv/index.js
+++ kctv/index.js
@@ -1,22 +1,40 @@
 // 메인 자동 슬라이더 및 버튼 슬라이더
-const mySwiper = new Swiper(".mySwiper", {
+const mySwiper = new Swiper('.mySwiper', {
   autoplay: {
     delay: 3000,
     centeredSlides: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
 
+// 유튜브
+const videoLinks = document.querySelectorAll('.vod-list a');
+videoLinks.forEach((link) => {
+  link.addEventListener('click', handleVideoClick);
+});
+function handleVideoClick(event) {
+  event.preventDefault();
+  const videoUrl = event.currentTarget.getAttribute('data-video-url');
+  showVideo(videoUrl);
+}
+function getBigScreenIframe() {
+  return document.querySelector('.youtube');
+}
+function showVideo(videoUrl) {
+  const bigScreenIframe = getBigScreenIframe();
+  bigScreenIframe.src = videoUrl;
+}
+
 // 자주하는 질문 상세 슬라이드
-const lis = document.querySelectorAll(".question-box > li");
+const lis = document.querySelectorAll('.question-box > li');
 lis.forEach(function (a, idx) {
-  $(a).on("click", () => {
-    $(a).find(".question-wrapper").slideToggle();
-    $(a).siblings().find(".question-wrapper").slideUp();
-    $(a).toggleClass("rotate");
-    $(a).siblings().removeClass("rotate");
+  $(a).on('click', () => {
+    $(a).find('.question-wrapper').slideToggle();
+    $(a).siblings().find('.question-wrapper').slideUp();
+    $(a).toggleClass('rotate');
+    $(a).siblings().removeClass('rotate');
   });
 });
