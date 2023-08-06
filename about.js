5fb2f8cb33ea345059f1d394513fe12bfc935e4f
yunyeonjeong <duswjddl0817@naver.com>
2023-08-06 21:06:01 +0900

    230806 2105 수정

--- about.js
+++ about.js
@@ -1,61 +1,61 @@
-//Loading
-const loader = document.querySelector('.loader');
-const html = document.querySelector('html');
+// about Loading
+const loader = document.querySelector(".loader");
+const html = document.querySelector("html");
 
-html.style.overflow = 'hidden';
+html.style.overflow = "hidden";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   setTimeout(() => {
-    loader.style.opacity = '0';
-    html.style.overflow = 'auto';
+    loader.style.opacity = "0";
+    html.style.overflow = "auto";
 
     setTimeout(() => {
-      loader.style.display = 'none';
+      loader.style.display = "none";
     }, 400);
   }, 2000);
 });
 
-// 기술 스택 박스
-const tBox = document.querySelectorAll('.t-box');
-const tBody = document.querySelector('body').offsetHeight;
+// about 기술 스택 박스
+const tBox = document.querySelectorAll(".t-box");
+const tBody = document.querySelector("body").offsetHeight;
 const tvh = document.documentElement.clientHeight;
-const tBoxVh = document.querySelector('.t-box').offsetHeight;
+const tBoxVh = document.querySelector(".t-box").offsetHeight;
 
-$(window).on('scroll', () => {
+$(window).on("scroll", () => {
   if (scrollY > tBody * (1 / 5)) {
     tBox[1].style.transform = `translateY(${-tBoxVh - tvh * (1 / 5)}px)`;
-    tBox[1].style.opacity = '1';
-    tBox[0].style.opacity = '0';
+    tBox[1].style.opacity = "1";
+    tBox[0].style.opacity = "0";
   } else {
     tBox[1].style.transform = `translateY(0px)`;
-    tBox[1].style.opacity = '0';
-    tBox[0].style.opacity = '1';
+    tBox[1].style.opacity = "0";
+    tBox[0].style.opacity = "1";
   }
   if (scrollY > tBody * (2 / 5)) {
     tBox[2].style.transform = `translateY(${(-tBoxVh - tvh * (1 / 5)) * 2}px)`;
-    tBox[2].style.opacity = '1';
-    tBox[1].style.opacity = '0';
+    tBox[2].style.opacity = "1";
+    tBox[1].style.opacity = "0";
   } else {
     tBox[2].style.transform = `translateY(0px)`;
-    tBox[2].style.opacity = '0';
-    tBox[1].style.opacity = '1';
+    tBox[2].style.opacity = "0";
+    tBox[1].style.opacity = "1";
   }
   if (scrollY > tBody * (3 / 5)) {
     tBox[3].style.transform = `translateY(${(-tBoxVh - tvh * (1 / 5)) * 3}px)`;
-    tBox[3].style.opacity = '1';
-    tBox[2].style.opacity = '0';
+    tBox[3].style.opacity = "1";
+    tBox[2].style.opacity = "0";
   } else {
     tBox[3].style.transform = `translateY(0px)`;
-    tBox[3].style.opacity = '0';
-    tBox[2].style.opacity = '1';
+    tBox[3].style.opacity = "0";
+    tBox[2].style.opacity = "1";
   }
   if (scrollY > tBody * (4 / 5)) {
     tBox[4].style.transform = `translateY(${(-tBoxVh - tvh * (1 / 5)) * 4}px)`;
-    tBox[4].style.opacity = '1';
-    tBox[3].style.opacity = '0';
+    tBox[4].style.opacity = "1";
+    tBox[3].style.opacity = "0";
   } else {
     tBox[4].style.transform = `translateY(0px)`;
-    tBox[4].style.opacity = '0';
-    tBox[3].style.opacity = '1';
+    tBox[4].style.opacity = "0";
+    tBox[3].style.opacity = "1";
   }
 });
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-//메인 시간
-const date = document.querySelector('.date span');
-const hours = document.querySelector('.hours span');
-const minutes = document.querySelector('.minutes span');
-const seconds = document.querySelector('.seconds span');
+// index 메인 시간
+const date = document.querySelector(".date span");
+const hours = document.querySelector(".hours span");
+const minutes = document.querySelector(".minutes span");
+const seconds = document.querySelector(".seconds span");
 const now = new Date();
 
 function updateTime() {
   const now = new Date();
-  date.textContent = String(now.getDate()).padStart(2, '0');
-  hours.textContent = String(now.getHours()).padStart(2, '0');
-  minutes.textContent = String(now.getMinutes()).padStart(2, '0');
-  seconds.textContent = String(now.getSeconds()).padStart(2, '0');
+  date.textContent = String(now.getDate()).padStart(2, "0");
+  hours.textContent = String(now.getHours()).padStart(2, "0");
+  minutes.textContent = String(now.getMinutes()).padStart(2, "0");
+  seconds.textContent = String(now.getSeconds()).padStart(2, "0");
 
   setTimeout(updateTime, 1000);
 }
 updateTime();
 
-//font 위로 올라 오는 값
+// index font 위로 올라 오는 값
 AOS.init();
